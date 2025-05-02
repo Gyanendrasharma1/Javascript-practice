1de0f6c4f24722707ffb630098e137ef75e6248d
Gyanendra <gyanendramail04@gmail.com>
2025-05-02 09:21:01 -0400

    Javascripts practice

---
+++ ao.js
@@ -0,0 +1,25 @@
+// Arithmetic Operators
+let x = 10;
+x += 10;
+console.log(x);
+
+let  y = 20;
+y -= 5;
+console.log(y);
+
+let z = 10;
+z *= 10;
+console.log(z);
+
+let a = 10;
+a /= 4;
+console.log(a);
+
+let b = 10;
+b %= 3;
+console.log(b);
+
+let c = 10;
+c **= 2;
+console.log(c);
+
---
+++ co.js
@@ -0,0 +1,40 @@
+// Comparison Operators in JavaScript
+
+// STEP-1 equality operator (==)
+let a = 90;
+let b = 10;
+
+console.log("== equal to:");
+console.log(a==b);
+
+//STEP-2 strict equality operator (===)
+
+console.log("=== strict equal to:");
+console.log(a===b);
+
+//STEP-3 not equal to (!=)
+console.log("!= not equal to:");
+console.log(a!=b);
+
+// STEP-4 strict not equal to (!==)
+console.log("!== not equal to:");
+console.log(a!=b);
+
+// STEP-5 greater than (>)
+console.log("> greater than:");
+console.log(a>b);
+
+// STEP-6 less than (<)
+console.log("< less than:");
+console.log(a<b);
+
+// STEP-7 greater than or equal to (>=)
+console.log(">= greater than or equal to:");
+console.log(a>=b);
+
+// STEP-8 less than or equal to (<=)
+console.log("<= lesser than or equal to:");
+console.log(a<=b);
+
+
+
